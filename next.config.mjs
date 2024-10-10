a2127433053bc29d3404f978f1a491e853868ce1
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-10-10 23:12:51 +0300

    add translation for project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const widthNextIntl = createNextIntlPlugin()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default widthNextIntl(nextConfig);
